864f844e575f3f894acab9bccaf255c61925e16a
Mustafa Sarshar <mustafa.sarshar88@gmail.com>
2022-02-13 12:07:44 +0100

    update

--- utils/methods.py
+++ utils/methods.py
@@ -10,18 +10,10 @@ def scale_dim(img, scale_factor=0.5, keep_aspect_ratio=False, fixed_height=170, 
             height_percent = (fixed_height / float(height))
             width = int((float(width) * float(height_percent)))
             height = fixed_height
-            
-            width_percent = (fixed_width / float(width))
-            height = int((float(height) * float(width_percent)))
-            width = fixed_width
     else:
         if keep_aspect_ratio and fixed_width:
             width_percent = (fixed_width / float(width))
             height = int((float(height) * float(width_percent)))
             width = fixed_width
-            
-            height_percent = (fixed_height / float(height))
-            width = int((float(width) * float(height_percent)))
-            height = fixed_height
 
     return (width, height)
