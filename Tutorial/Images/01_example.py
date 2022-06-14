e4fe4245489b5007b862733c49c79763cd151e96
muSSar <mustafa.sarshar88@gmail.com>
2022-06-14 17:13:24 +0200

    update

---
+++ Tutorial/Images/01_example.py
@@ -0,0 +1,15 @@
+"""
+Getting Started with Images
+https://docs.opencv.org/4.6.0/db/deb/tutorial_display_image.html
+"""
+
+import cv2 as cv
+import sys
+img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+if img is None:
+    sys.exit("Could not read the image.")
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+if k == ord("s"):
+    cv.imwrite("starry_night.png", img)
+
